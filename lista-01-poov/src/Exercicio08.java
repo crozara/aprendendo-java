6f92adcf460e051814db8b4def1a1b3d3378e74e
Kauê Crozara <ikauecrozara@gmail.com>
2026-08-10 18:10:10 -0300

    refactor: formatação de impressão de respostas

--- lista-01-poov/src/Exercicio08.java
+++ lista-01-poov/src/Exercicio08.java
@@ -13,33 +13,33 @@ public class Exercicio08 {
         Scanner s = new Scanner(System.in);
 
         for(int i = 0; i < 10; i++){
-            System.out.println("Digite um valor: ");
+            System.out.print("Digite um valor: ");
             vetor[i] = s.nextInt();   
+            System.out.println("Valor inserido: " + vetor[i] + "\n");
         }
 
 
         // c) Preencher os 10 últimos valores do vetor com números aleatórios entre 1 e 100
-
+        System.out.println("Inserindo 10 números aleatórios de 1 a 100...");
         Random r = new Random();
         for(int i = vetor.length - 10; i <vetor.length; i++){
             vetor[i] = r.nextInt(1, 100); 
         }
+        System.out.println("\nValores inseridos!");
 
         // d) Mostrar o vetor inteiro na tela
 
-        System.out.println("O vetor atual: ");
+        System.out.println("\nO vetor atual: ");
         for(int i = 0; i < vetor.length; i++){
             System.out.printf("%d ", vetor[i]);
         }
-        System.out.print("\n");
 
         // e) Mostrar o vetor de trás para frente na tela
 
-        System.out.println("O vetor de trás para frente: ");
+        System.out.println("\nO vetor de trás para frente: ");
         for(int i = vetor.length - 1; i >= 0; i--){
             System.out.printf("%d ", vetor[i]);
         }
-        System.out.print("\n");
             
         // f) Calcular e mostrar a média dos valores do vetor
 
@@ -47,7 +47,7 @@ public class Exercicio08 {
         for(int i = 0; i < vetor.length; i++){
             soma += vetor[i];
         }
-        System.out.printf("Média entre os números do vetor: %.2f\n", soma/(vetor.length));
+        System.out.printf("\n\nMédia entre os números do vetor: %.2f\n\n", soma/(vetor.length));
         
         // g) Encontrar e mostrar o maior valor e o menor valor do vetor 
 
@@ -67,27 +67,31 @@ public class Exercicio08 {
 
         // h) Mostrar os valores das posições pares do vetor
         
+        System.out.print("\nValores das posições pares:");
         for (int i = 0; i < vetor.length; i += 2){
-            System.out.println("Valores das posições pares: " + vetor[i]);
+            System.out.print(" " + vetor[i]);
         }
         // i) Mostrar os valores das posições ímpares do vetor
 
+        System.out.print("\nValores das posições ímpares:");
         for(int i = 1; i < vetor.length; i += 2){
-            System.out.println("Valores das posições ímpares: " + vetor[i]);
+            System.out.print(" " + vetor[i]);
         }
 
         // j) Mostrar os valores pares do vetor
 
+        System.out.print("\n\nValores pares do vetor:");
         for(int i = 0; i < vetor.length; i++){
             if(vetor[i] % 2 == 0){
-                System.out.println("Valores pares do vetor: " + vetor[i]);
+                System.out.print(" " + vetor[i]);
             }
         }
         // k) Mostrar os valores ímpares do vetor
 
+        System.out.print("\nValores ímpares do vetor:");
         for(int i = 0; i < vetor.length; i++){
             if(vetor[i] % 2 != 0){
-                System.out.println("Valores ímpares do vetor: " + vetor[i]);
+                System.out.print(" " + vetor[i]);
             }
         }
         // l)Verificar se há elementos repetidos no vetor e exibir uma mensagem em caso afirmativo
@@ -104,13 +108,13 @@ public class Exercicio08 {
         }
         if(ans != 0)
         {
-            System.out.println("Existe vetor repetido no vetor!");
+            System.out.println("\n\nExiste valor repetido no vetor!");
         }
 
         // m) Solicitar um valor ao usuário e verificar se esse valor existe no vetor. Se existir exibir a posição em que ele está, senão exibir uma mensagem de valor não encontrado 
         int valor = 0;
         int encontrado = 0;
-        System.out.print("Digite um valor para averiguar sua existência: ");
+        System.out.print("\nDigite um valor para averiguar sua existência: ");
         valor = s.nextInt();
 
         for(int i = 0; i < vetor.length; i++){
