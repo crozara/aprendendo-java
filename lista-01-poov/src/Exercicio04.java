003c544b5eb9335c594c160eee197152763d6419
Kauê Crozara <ikauecrozara@gmail.com>
2026-08-16 12:15:26 -0300

    feat: adiciona Exercicio 04 buscando todos os primos em um intervalo

---
+++ lista-01-poov/src/Exercicio04.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Digite um número e descubra todos os primos até ele: ");
+        int cont = 0;
+        int limite = s.nextInt();
+
+        for(int i = 2; i <= limite; i++){
+            cont = 0;
+
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    cont++;
+                    if(cont > 2){
+                        break;
+                    }
+                }
+            }
+            if(cont == 2){
+                System.out.print(i + " ");
+            }
+        }
+        
+        System.out.println();
+        s.close();
+    }
+}
